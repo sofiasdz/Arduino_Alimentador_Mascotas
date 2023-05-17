ed218e9d7ce1b2e31787f9afc33fbd3998f40d69
jalopbauer <jorge.lopezbauer@ing.austral.edu.ar>
2023-05-17 12:44:43 -0300

    Codigo ordenado

--- Comedor_Codigo.ino
+++ Comedor_Codigo.ino
@@ -1,81 +1,8 @@
-#include "EasyBuzzer.h"
 #include <ESP32_Servo.h>
 #include <virtuabotixRTC.h>
-#include <Tone32.h>
 
 
-//Buzzer
-int buzzerPin = 16;
-const int canalBuzzer = 1;
-
-
-//Servo
-Servo miServo;  // Creo un objeto servo para controlarlo
-int pos = 0;    // variable que guarda la posicion
-int servoPin = 4;
-const int canalServo = 0;
-
-
-//Clock
-virtuabotixRTC myRTC(21, 22, 23);
-
-void done() {
-  Serial.print("Done!");
-}
-
-boolean timeForFood() {
-  return myRTC.seconds == 10 || myRTC.seconds == 20 || myRTC.seconds == 30 || myRTC.seconds == 40 || myRTC.seconds == 50 || myRTC.seconds == 60;
-}
-
-void buzzerMakeSound() {
-  digitalWrite(buzzerPin, HIGH);  // suena
-  delay(500);
-  digitalWrite(buzzerPin, LOW);  // no suena
-}
-
-void releaseFood() {
-  miServo.write(180);
-  delay(500);
-  miServo.write(0);
-}
-
-void setupBuzzer() {
-  pinMode(buzzerPin, OUTPUT);
-  //ledcAttachPin(buzzerPin, canalBuzzer);
-}
-
-void setupServo() {
-  pinMode(servoPin, OUTPUT);
-  miServo.attach(servoPin);  // Defino en que pin va conectado el servo
-}
-
-void setupClock() {
-  Serial.begin(9600);
-  // segundos, minutos, horas, dia de la semana, numero de día, mes y año
-  // myRTC.setDS1302Time(00, 39, 11, 03, 10, 05, 2023);
-}
-
-void updateClock() {
-  // Esta función actualiza las variables para obtener resultados actuales
-  myRTC.updateTime();
-
-  // Se imprime el resultado en el Monitor Serial
-  Serial.print("Fecha y hora actual: ");
-  Serial.print(myRTC.dayofmonth);  // Se puede cambiar entre día y mes si se utiliza el sistema Americano
-  Serial.print("/");
-  Serial.print(myRTC.month);
-  Serial.print("/");
-  Serial.print(myRTC.year);
-  Serial.print(" ");
-  Serial.print(myRTC.hours);
-  Serial.print(":");
-  Serial.print(myRTC.minutes);
-  Serial.print(":");
-  Serial.println(myRTC.seconds);
-
-  // Un pequeño delay para no repetir y leer más facil
-  delay(1000);
-}
+// Setup comedor
 
 void setup() {
   Serial.begin(9600);
@@ -84,8 +11,53 @@ void setup() {
   setupClock();
 }
 
+// Servo
+int buzzerPin = 16;
+const int canalBuzzer = 1;
+void setupBuzzer() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+// Buzzer
+Servo miServo;  // Creo un objeto servo para controlarlo
+int pos = 0;    // variable que guarda la posicion
+int servoPin = 4;
+const int canalServo = 0;
+
+void setupServo() {
+  pinMode(servoPin, OUTPUT);
+  miServo.attach(servoPin); 
+}
+
+virtuabotixRTC myRTC(21, 22, 23);
+
+void setupClock() {
+  Serial.begin(9600);
+  // myRTC.setDS1302Time(00, 39, 11, 03, 10, 05, 2023);
+}
+
+boolean timeForFood() {
+  return myRTC.seconds == 10 || myRTC.seconds == 20 || myRTC.seconds == 30 || myRTC.seconds == 40 || myRTC.seconds == 50 || myRTC.seconds == 60;
+}
+
+void buzzerMakeSound() {
+  digitalWrite(buzzerPin, HIGH);
+  delay(500);
+  digitalWrite(buzzerPin, LOW);
+}
+
+void releaseFood() {
+  miServo.write(180);
+  delay(500);
+  miServo.write(0);
+}
+
+void loopClock() {
+  myRTC.updateTime();
+}
+
 void loop() {
-  updateClock();
+  loopClock();
   if (timeForFood()) {
     releaseFood();
     delay(500);
